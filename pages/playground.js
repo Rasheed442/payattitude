74100fc3103d3f4446fd8b653b94d9944ddf9452
Rasheed <abdulraufrasheed15@gmail.com>
2024-02-24 13:49:55 +0100

    other pages

--- pages/playground.js
+++ pages/playground.js
@@ -1,29 +1,7 @@
 import React from "react";
 
 function playground() {
-  return (
-    <>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        effect={"fade"}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation, EffectFade]}
-        className={style.swiper}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-      </Swiper>
-    </>
-  );
+  return <></>;
 }
 
 export default playground;
